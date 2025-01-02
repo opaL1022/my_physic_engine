d52b942e3f5e886e8b9b2033e3a6154baed62d0a
opaL1022 <whitestoneabc123@gmail.com>
2025-01-03 05:55:10 +0800

    新增VAO VBO EBO
    完成 VAO VBO EBO 的.cpp與.h檔

--- src/VBO.h
+++ src/VBO.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef VBO_CLASS_H
+#define VBO_CLASS_H
+
+#include<glad/glad.h>
+
+class VBO
+{
+	public:
+		GLuint ID;
+		
+		VBO(GLuint* vertices, GLsizeiptr size);
+
+		void Bind();
+		void Unbind();
+		void Delete();
+};
+
+#endif
